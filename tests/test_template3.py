8cd23f239ba6670ce5f37ba5ce40767e94c7db30
Tom Elliott <tom.elliott@nyu.edu>
2018-05-11 04:33:14 -0500

    test template

---
+++ tests/test_template3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Python 3 tests template (changeme)"""
+
+import logging
+from nose.tools import assert_equal, assert_false, assert_true, raises
+from os.path import abspath, join, realpath
+from unittest import TestCase
+
+logger = logging.getLogger(__name__)
+test_data_path = abspath(realpath(join(['tests', 'data'])))
+
+
+def setup_module():
+    """Change me"""
+    pass
+
+
+def teardown_module():
+    """Change me"""
+    pass
+
+
+class Test_This(TestCase):
+
+    def setUp(self):
+        """Change me"""
+        pass
+
+    def tearDown(self):
+        """Change me"""
+        pass
+
+    def test_a(self):
+        """Change me"""
+        pass
